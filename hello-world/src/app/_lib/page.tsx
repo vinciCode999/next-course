1d14aad13fba6dcc7ee9df2ed68f33ed8e9e0772
vinci999 <124420382+vinciCode999@users.noreply.github.com>
2025-09-07 08:18:57 +0200

    lesson7: private folders

---
+++ hello-world/src/app/_lib/page.tsx
@@ -0,0 +1,7 @@
+export default function PrivateRoute() {
+  //for private route save the folder starting with underscore(_foldername)
+  //underscore=== url encoded: %5F => _
+  return (
+    <div>you can not view this in the browser.</div>
+  )
+}
