c41f162e31651e4e5543a18526f830d476065e47
vinci999 <124420382+vinciCode999@users.noreply.github.com>
2025-09-07 07:49:42 +0200

    lesson6: file colocation

---
+++ hello-world/src/app/dashboard/line-chart.tsx
@@ -0,0 +1,3 @@
+export default function LineChart(){
+  return <h2>Line Chart.</h2>
+}
---
+++ hello-world/src/app/dashboard/page.tsx
@@ -0,0 +1,8 @@
+function BarChart(){
+  return <h2>Bar Chart.</h2>
+}
+//always make sure you export a page as default
+
+export default function Dashboard(){
+  return <h1>Dashboard</h1>;
+}
